fbe9b336cc96c47b8bf887f6269fed088dd1d877
Daniel Choi <schoiak@connect.ust.hk>
2023-09-21 17:51:57 +0900

    fix: required field modified to optional

--- visualizer/weights_dto.py
+++ visualizer/weights_dto.py
@@ -2,16 +2,16 @@ from pydantic import BaseModel
 
 
 class Weights(BaseModel):
-    광배근: int
-    기립근: int
-    대퇴사두: int
-    대흉근: int
-    둔근: int
-    삼두: int
-    승모근: int
-    이두근: int
-    전면어깨: int
-    측면어깨: int
-    코어: int
-    햄스트링: int
-    후면어깨: int
+    광배근: int = 0
+    기립근: int = 0
+    대퇴사두: int = 0
+    대흉근: int = 0
+    둔근: int = 0
+    삼두: int = 0
+    승모근: int = 0
+    이두근: int = 0
+    전면어깨: int = 0
+    측면어깨: int = 0
+    코어: int = 0
+    햄스트링: int = 0
+    후면어깨: int = 0
